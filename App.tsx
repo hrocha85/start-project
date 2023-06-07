17cbbbb686c3f81fe32da79f86d39cef42e9cc60
LucasBGarcia <barbozagarcia@yahoo.com.br>
2023-06-07 17:04:16 -0300

    configs iniciais

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, JosefinSlab_400Regular } from '@expo-google-fonts/josefin-slab'
+import { Routes } from './src/routes';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    JosefinSlab_400Regular
+  })
+
+  if (!fontsLoaded) {
+    return (
+      <Text>carregando fonte</Text>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <>
+      <Routes />
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
 
